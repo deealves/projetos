a81c899727ce7c07272da27f348cbcafd48497d9
Dennys Alves <dennysangelim.jrf@gmail.com>
2019-11-17 14:02:59 -0300

    Interface Produto

---
+++ Projeto_de_Produtos_MAP/Projeto_de_Produtos_MAP/src/br/com/map/projeto_produtos/util/ConnectionFactory.java
@@ -0,0 +1,27 @@
+
+package br.com.map.projeto_produtos.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Dennys Alves
+ */
+
+public class ConnectionFactory {
+    
+    private static final String URL = "jdbc:postgresql://localhost:5432/produtos";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "admin";
+    
+    public static Connection getConnection(){
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            return null;
+        }
+    }
+}
